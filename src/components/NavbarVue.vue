61c0ea039fa42dff6cb5ea86266c9b7e6dfa542b
JHKIMS <wjdgns5131@gmail.com>
2022-12-02 19:20:00 +0900

    Vue2_Router3: <router-link>사용+navbar생성성

---
+++ src/components/NavbarVue.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+        <router-link to="/">Home</router-link>
+        <router-link to="/Login">Login</router-link><!-- router-link는 화면을 갱신하지 않는다.-->
+    </div>
+</template>
+<script>
+export default {
+
+}
+</script>
+<style>
+
+</style>
