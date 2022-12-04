bb778bc660b48e115b3a9f36504746b82d136ac2
JHKIMS <wjdgns5131@gmail.com>
2022-12-04 10:12:53 +0900

    Vue2_Router3: 라우터 마지막_데이터 불러오기

--- src/components/BoardVue.vue
+++ src/components/BoardVue.vue
@@ -1,24 +1,39 @@
 <template>
-    <div>
-        boardId: {{boardId}}
-        <router-link :to="`/b/${boardId}/c/1`">Card_1</router-link>
-        <router-link :to="`/b/${boardId}/c/2`">Card_2</router-link>
-        <hr />
-        <router-view></router-view>
+  <div>
+    Board
+    <div v-if="loading">loading board...</div>
+
+    <div v-else>
+      boardId: {{ boardId }}
+      <router-link :to="`/b/${boardId}/c/1`">Card_1</router-link>
+      <router-link :to="`/b/${boardId}/c/2`">Card_2</router-link>
     </div>
+
+    <hr />
+    <router-view></router-view>
+  </div>
 </template>
 <script>
 export default {
-    data(){
-        return{
-            boardId: 0
-        }
-    },
-    created(){
-        this.boardId = this.$route.params.boardId;
+  data() {
+    return {
+      boardId: 0,
+      loading: false,
+    };
+  },
+  created() {
+    this.fetchData();
+  },
+  methods: {
+    fetchData(){
+        // fetchData: 백엔드API를 호출하고, 요청하는 데이터
+        this.loading = true;
+        setTimeout(()=>{
+            this.boardId = this.$route.params.boardId
+            this.loading = false
+        },500)
     }
-}
+  }
+};
 </script>
-<style>
-    
-</style>
+<style></style>
--- src/components/CardVue.vue
+++ src/components/CardVue.vue
@@ -1,26 +1,40 @@
 <template>
-    <div>
-        CardVue
-        <div>cardId: {{cardId}}</div>
+  <div>
+    CardVue
+
+    <div v-if="loading">loading card...</div>
+    <div v-else>
+      <div>cardId: {{ cardId }}</div>
     </div>
+  </div>
 </template>
 <script>
 export default {
-    data(){
-        return {
-            cardId: 0
-        }
-    },
-    watch:{
-        '$route'(){
-            this.cardId = this.$route.params.cardId;
-        }
-    },
-    created(){ // 카드정보를 갱신하는 부분
-        this.cardId = this.$route.params.cardId;
+  data() {
+    return {
+      cardId: 0,
+      loading: false
+    };
+  },
+  watch: {
+    $route: {
+    //   this.fetchData();
+        handler: 'fetchData',
+        immediate: true
     }
-}
+  },
+/*   created() {
+    this.fetchData();
+  }, */
+  methods: {
+    fetchData() {
+      this.loading = true;
+      setTimeout(() => {
+        this.cardId = this.$route.params.cardId;
+        this.loading = false;
+      }, 500);
+    }
+  }
+};
 </script>
-<style>
-    
-</style>
+<style></style>
