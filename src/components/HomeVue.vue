c63db13ddd01c057243d020cae0df86fa9c69d81
JHKIMS <wjdgns5131@gmail.com>
2022-12-04 16:48:24 +0900

    Vue2_Router3: 순수 브라우저 API사용연습

--- src/components/HomeVue.vue
+++ src/components/HomeVue.vue
@@ -1,24 +1,52 @@
 <template>
-    <div>
-        Home_BoardList
-        <ul>
-            <li>
-                <router-link to="/b/1">Board_1</router-link>
-            </li>
-            <li>
-                <router-link to="/b/2">Board_2</router-link>
-            </li>
-            <li>
-                <router-link to="/b/3">Board_3</router-link>
-            </li>
-        </ul>
-    </div>
+  <div>
+    Home_BoardList?
+    <div v-if="loading">Loading...</div>
+    <div v-else>Api Result : {{ apiRes }}</div>
+
+    <ul>
+      <li>
+        <router-link to="/b/1">Board_1</router-link>
+      </li>
+      <li>
+        <router-link to="/b/2">Board_2</router-link>
+      </li>
+      <li>
+        <router-link to="/b/3">Board_3</router-link>
+      </li>
+    </ul>
+  </div>
 </template>
 <script>
 export default {
-    
-}
+  data() {
+    return {
+      loading: false,
+      apiRes: "",
+    };
+  },
+  created() {
+    this.fetchData();
+  },
+  methods: {
+    fetchData() {
+      this.loading = true;
+      const req = new XMLHttpRequest();
+
+      req.open("GET", "http://localhost:3000/health");
+
+      req.send();
+
+      req.addEventListener("load", () => {
+        this.loading = false;
+        this.apiRes = {
+          status: req.status,
+          statusText: req.statusText,
+          response: JSON.parse(req.response),
+        };
+      });
+    },
+  },
+};
 </script>
-<style>
-    
-</style>
+<style></style>
